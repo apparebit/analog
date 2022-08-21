6af2a1623a092b6a81ea54f65e3599945785db01
Robert Grimm <apparebit@gmail.com>
2022-08-21 15:05:09 -0400

    Rewrite analyzer as fluent builder with more flexible phrasing

--- analog/analyzer.py
+++ analog/analyzer.py
@@ -1,180 +1,210 @@
 from __future__ import annotations
-from typing import Any
+from typing import Any, Callable, Optional, TypeVar
 
 import pandas as pd
 
+from .data_manager import Coverage
 from .label import ContentType, HttpMethod, HttpStatus, HttpStatus
 
 
-def calculate(df: pd.DataFrame) -> FrameAnalyzer:
-    """
-    Turn the dataframe into a fluent log analyzer. To simplify analysis, this
-    function updates the dataframe's index to its `timestamp` column. It does so
-    destructively, in place.
-    """
-    if "timestamp" in df.columns:
-        df.set_index("timestamp", inplace=True)
-    return FrameAnalyzer(df)
+SeriesWrapper = TypeVar('SeriesWrapper', bound='Seriesful')
+FrameWrapper = TypeVar('FrameWrapper', bound='Frameful')
+
+SeriesMapper = Callable[[pd.DataFrame], pd.Series]
+FrameMapper = Callable[[pd.DataFrame], pd.DataFrame]
 
 
-class FrameAnalyzer:
-    def __init__(self, df: pd.DataFrame) -> None:
-        self._frame = df
-
-    @property
-    def frame(self) -> pd.DataFrame:
-        return self._frame
-
-    # ----------------------------------------------------------------------------------
-    # Subject to denote bots or humans
-
-    def bots(self) -> FrameAnalyzer:
-        """Accept requests from bots only."""
-        return FrameAnalyzer(self._frame[self._frame["is_bot"]])
-
-    def humans(self) -> FrameAnalyzer:
-        """Accept requests from humans only."""
-        return FrameAnalyzer(self._frame[~self._frame["is_bot"]])
-
-    # ----------------------------------------------------------------------------------
-    # Adverb to denote the rough outcome
-
-    def successfully(self) -> FrameAnalyzer:
-        """
-        Accept successful requests with a 2xx status code indicating
-        successful completion only.
-        """
-        return FrameAnalyzer(
-            self._frame[self._frame["status_class"] == HttpStatus.SUCCESSFUL]
-        )
-
-    def inaccurately(self) -> FrameAnalyzer:
-        """
-        Accept requests with a 3xx status code indicating redirection only.
-        """
-        return FrameAnalyzer(
-            self._frame[self._frame["status_class"] == HttpStatus.REDIRECTION]
-        )
-
-    def incorrectly(self) -> FrameAnalyzer:
-        """
-        Accept incorrect requests with a 4xx status code indicating a client
-        error only.
-        """
-        return FrameAnalyzer(
-            self._frame[self._frame["status_class"] == HttpStatus.CLIENT_ERROR]
-        )
-
-    def unsuccessfully(self) -> FrameAnalyzer:
-        """
-        Accept unsuccessful requests with a 5xx status code indicating server
-        error only.
-        """
-        return FrameAnalyzer(
-            self._frame[self._frame["status_class"] == HttpStatus.SERVER_ERROR]
-        )
-
-    # ----------------------------------------------------------------------------------
-    # Verb to denote the HTTP method
-
-    def getting(self) -> FrameAnalyzer:
-        """Filter out requests with methods other than GET."""
-        return FrameAnalyzer(
-            self._frame[self._frame["method"] == HttpMethod.GET]
-        )
-
-    def posting(self) -> FrameAnalyzer:
-        """Filter out requests with methods other than POST."""
-        return FrameAnalyzer(
-            self._frame[self._frame["method"] == HttpMethod.POST]
-        )
-
-    # ----------------------------------------------------------------------------------
-    # Optional adjective to denote specific outcome
-
-    def non_existent(self) -> FrameAnalyzer:
-        """Accept only requests resulting in a 404 Not Found status."""
-        return FrameAnalyzer(
-            self._frame[self._frame["status"] == 404]
-        )
-
-    # ----------------------------------------------------------------------------------
-    # Object to denote the type of content
-
-    def resources(self) -> FrameAnalyzer:
-        """Accept all content."""
-        return self
-
-    def content(self, ct: ContentType) -> FrameAnalyzer:
-        """Filter out content other than the given content type."""
-        return FrameAnalyzer(
-            self._frame[self._frame["content_type"] == ct.value]
-        )
-
-    def markup(self) -> FrameAnalyzer:
-        """Filter out content other than HTML markup."""
-        return FrameAnalyzer(
-            self._frame[self._frame["content_type"] == ContentType.MARKUP]
-        )
-
-    # ----------------------------------------------------------------------------------
-    # Adverb to denote calculation frequency
-
-    def per_month(self) -> SeriesAnalyzer:
-        """Count the number of requests per month as a time series (not frame)."""
-        # M is end of month and MS is start of month.
-        return SeriesAnalyzer(
-            self._frame.resample("MS").size()  # type: ignore[no-untyped-call]
-        )
-
-    def per_day(self) -> SeriesAnalyzer:
-        """Count the number of requests per day as a time series (not frame)."""
-        return SeriesAnalyzer(
-            self._frame.resample("D").size()  # type: ignore[no-untyped-call]
-        )
-
-    # ----------------------------------------------------------------------------------
-    # New sentence to denote output
-
-    def then_print(self) -> FrameAnalyzer:
-        """Print the result to standard output."""
-        print(self._frame.to_string())
-        return self
-
-    def then_plot(self, **kwargs: object) -> FrameAnalyzer:
-        """Plot the result."""
-        self._frame.plot(**kwargs)
-        return self
-
-
-class SeriesAnalyzer:
-    def __init__(self, series: pd.Series) -> None:
+class Seriesful:
+    def __init__(self, series: pd.Series, cover: Coverage) -> None:
         self._series = series
+        self._cover = cover
 
     @property
     def series(self) -> pd.Series:
         return self._series
 
-    def then_plot(self) -> SeriesAnalyzer:
-        self._series.plot()
+    @property
+    def coverage(self) -> Coverage:
+        return self._cover
+
+
+class FluentSeriesDisplay(Seriesful):
+    def then_print(self, entries: Optional[int] = None) -> FluentSeriesDisplay:
+        if entries is not None:
+            print(self._series.head(entries))
+        else:
+            print(self._series.to_string())
+        return self
+
+    def then_plot(self, **kwargs: object) -> FluentSeriesDisplay:
+        self._series.plot(**kwargs)
         return self
 
 
+# ======================================================================================
 
-#     ts = df[timestamp]
-#     ts_min = ts.min()
-#     ts_max = ts.max()
-#     result = df.groupby([ts.dt.year, ts.dt.month, third_column]).aggregate(aggregate)
-#     result = result[
-#         (ts_min.year, ts_min.month, sc_min):(ts_max.year, ts_max.month, sc_max)
-#     ]
 
-# Metrics:
-#
-# * Total number of requests
-# * Requests broken down by status class for humans vs bots
-# * Requests broken down by content type for humans vs bots
-# * Views per page, i.e., successful non-bot GETs for markup per cool path.
-# * The same for all content
-# * Resources that were not found by human users, sorted by popularity.
-# * Referrals
+class Frameful:
+    def __init__(self, frame: pd.DataFrame, cover: Coverage) -> None:
+        self._frame = frame
+        self._cover = cover
+
+    @property
+    def frame(self) -> pd.DataFrame:
+        return self._frame
+
+    @property
+    def coverage(self) -> Coverage:
+        return self._cover
+
+    def handoff(self, wrapper: type[FrameWrapper]) -> FrameWrapper:
+        return wrapper(self._frame, self._cover)
+
+    def map(self, wrapper: type[FrameWrapper], mapper: FrameMapper) -> FrameWrapper:
+        return wrapper(mapper(self._frame), self._cover)
+
+    def filter(
+        self, wrapper: type[FrameWrapper], predicate: SeriesMapper
+    ) -> FrameWrapper:
+        return wrapper(self._frame[predicate(self._frame)], self._cover)
+
+    def serialize(
+        self, wrapper: type[SeriesWrapper], mapper: SeriesMapper
+    ) -> SeriesWrapper:
+        return wrapper(mapper(self._frame), self._cover)
+
+
+# --------------------------------------------------------------------------------------
+# Normally, FluentPhrase would be defined here. It starts fluent phrases after
+# all. Alas, it allows skipping straight to display and hence is defined at end
+# of module.
+
+
+class FluentFilter(Frameful):
+    def bots(self) -> FluentPhrase:
+        return self.filter(FluentPhrase, lambda df: df["is_bot"])
+
+    def humans(self) -> FluentPhrase:
+        return self.filter(FluentPhrase, lambda df: ~df["is_bot"])
+
+    def get(self) -> FluentPhrase:
+        return self.filter(FluentPhrase, lambda df: df["method"] == HttpMethod.GET)
+
+    def post(self) -> FluentPhrase:
+        return self.filter(FluentPhrase, lambda df: df["method"] == HttpMethod.POST)
+
+    # FIXME awkward method name
+    def has(self, content_type: ContentType) -> FluentPhrase:
+        return self.filter(FluentPhrase, lambda df: df["content_type"] == content_type)
+
+    def markup(self) -> FluentPhrase:
+        return self.filter(
+            FluentPhrase, lambda df: df["content_type"] == ContentType.MARKUP
+        )
+
+    def successful(self) -> FluentPhrase:
+        return self.filter(
+            FluentPhrase, lambda df: df["status_class"] == HttpStatus.SUCCESSFUL
+        )
+
+    def redirection(self) -> FluentPhrase:
+        return self.filter(
+            FluentPhrase, lambda df: df["status_class"] == HttpStatus.REDIRECTION
+        )
+
+    def client_error(self) -> FluentPhrase:
+        return self.filter(
+            FluentPhrase, lambda df: df["status_class"] == HttpStatus.CLIENT_ERROR
+        )
+
+    def not_found(self) -> FluentPhrase:
+        return self.filter(FluentPhrase, lambda df: df["status"] == 404)
+
+    def server_error(self) -> FluentPhrase:
+        return self.filter(
+            FluentPhrase, lambda df: df["status_class"] == HttpStatus.SERVER_ERROR
+        )
+
+
+class FluentDuration(Frameful):
+    def last_month(self) -> FluentBreakdown:
+        one_month_ago = self._frame["timestamp"].max() - pd.DateOffset(months=1)
+        return self.filter(FluentBreakdown, lambda df: df["timestamp"] > one_month_ago)
+
+    def last_year(self) -> FluentBreakdown:
+        one_year_ago = self._frame["timestamp"].max() - pd.DateOffset(years=1)
+        return self.filter(FluentBreakdown, lambda df: df["timestamp"] > one_year_ago)
+
+
+class FluentBreakdown(Frameful):
+    # FIXME: Need a way to bypass by(); rename by()
+    def by(self, column: str) -> FluentSeriesDisplay:
+        return self.serialize(FluentSeriesDisplay, lambda df: df[column].value_counts())
+
+
+class FluentRequestRate(Frameful):
+    def per_day(self) -> FluentSeriesDisplay:
+        return self.serialize(
+            FluentSeriesDisplay,
+            lambda df: df.resample('D', on='timestamp').size(),  # type: ignore [no-untyped-call]
+        )
+
+    def per_month(self) -> FluentSeriesDisplay:
+        return self.serialize(
+            FluentSeriesDisplay,
+            lambda df: df.resample('MS', on='timestamp').size(),  # type: ignore [no-untyped-call]
+        )
+
+
+class FluentFrameDisplay(Frameful):
+    def then_print(self, entries: Optional[int] = None) -> FluentFrameDisplay:
+        if entries is not None:
+            print(self._frame.head(entries))
+        else:
+            print(self._frame.to_string())
+        return self
+
+    def then_plot(self, **kwargs: object) -> FluentFrameDisplay:
+        self._frame.plot(**kwargs)
+        return self
+
+
+class FluentPhrase(FluentFrameDisplay):
+    @property
+    def only(self) -> FluentFilter:
+        """Filter out requests that do not meet the criterion."""
+        return self.handoff(FluentFilter)
+
+    @property
+    def during(self) -> FluentDuration:
+        """Filter out requests older than a month or a year."""
+        return self.handoff(FluentDuration)
+
+    @property
+    def in_requests(self) -> FluentRequestRate:
+        """Convert to requests per month or day."""
+        return self.handoff(FluentRequestRate)
+
+
+# ======================================================================================
+
+
+def analyze(frame: pd.DataFrame, cover: Coverage) -> FluentPhrase:
+    return FluentPhrase(frame, cover)
+
+
+def merge(columns: dict[str, Seriesful]) -> FluentPhrase:
+    """
+    Merge the named series as columns in a new, wrapped dataframe. All series
+    must have the same coverage, i.e., be derrived from the same original
+    dataframe.
+    """
+    for index, wrapper in enumerate(columns.values()):
+        if index == 0:
+            cover = wrapper.coverage
+        elif cover is not wrapper.coverage:
+            raise ValueError(f'Series {columns} have different coverage')
+
+    return FluentPhrase(
+        pd.concat([s.series.rename(n) for n, s in columns.items()], axis=1), cover
+    )
