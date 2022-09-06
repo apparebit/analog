13b70fcf203178b6e15127f1b8179c93ea6cfb51
Robert Grimm <apparebit@gmail.com>
2022-09-06 14:14:41 -0400

    Add dedicated error hierarchy covering parsing, data management, and validation

--- analog/error.py
+++ analog/error.py
@@ -1,2 +1,14 @@
-class StorageError(Exception):
-    pass
+class AnalogError(Exception):
+    """The base class for all of analog's errors."""
+
+
+class ParseError(AnalogError):
+    """An error indicating a malformed access log line."""
+
+
+class StorageError(AnalogError):
+    """An error indicating missing data directories or files."""
+
+
+class ValidationError(AnalogError):
+    """An error indicating a log dataframe inconsistent with its schema."""
