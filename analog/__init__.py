d566027e9fa10a97c4f4c139d4d29998cdde5192
Robert Grimm <apparebit@gmail.com>
2022-09-23 23:56:11 -0400

    Re-export core functionality for (Pythonic) use through main module

--- analog/__init__.py
+++ analog/__init__.py
@@ -1,1 +1,15 @@
+__all__ = (
+    '__version__',
+    'analyze',
+    'coerce',
+    'fresh_counts',
+    'latest',
+    'merge',
+    'validate',
+)
+
 __version__ = "0.1.0"
+
+from .analyzer import analyze, fresh_counts, merge
+from .data_manager import latest
+from .schema import coerce, validate
